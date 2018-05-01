821cc08cde691f0ca5a9a965a701ef472917d6be
Vegetto <github@angelnu.com>
2018-05-01 18:33:45 +0200

    Add manifest

--- build.sh
+++ build.sh
@@ -16,23 +16,6 @@ cd $(dirname $0)
 : ${QEMU_VERSION:="v2.11.1"}
 
 
-#Prepare qemu
-mkdir -p qemu
-cd qemu
-if [ ! -f qemu-"$TARGET_ARCH"-static ]; then
-  echo "Running in arch $(uname -m) and with TARGET_ARCH $TARGET_ARCH"
-  if [ "$TARGET_ARCH" = "amd64" -o "$TARGET_ARCH" = "$(uname -m)" ]; then
-    touch qemu-"$TARGET_ARCH"-static
-  else
-    # Prepare qemu
-    docker run --rm --privileged multiarch/qemu-user-static:register --reset
-    curl -L -o qemu-"$TARGET_ARCH"-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/"$QEMU_VERSION"/qemu-"$TARGET_ARCH"-static.tar.gz
-    tar xzf qemu-"$TARGET_ARCH"-static.tar.gz
-    rm qemu-"$TARGET_ARCH"-static.tar.gz
-  fi
-fi
-cd ..
-
 case $TARGET_ARCH in
 armv7l|arm)
   ARCH_TAG="${TAG}-arm"
@@ -49,8 +32,29 @@ x86_64|amd64)
   ;;
 esac
 
-if [ "$BUILD" = true ] ; then
+###############################
 
+if [ "$BUILD" = true ] ; then
+  echo "BUILDING DOCKER $REPO:$ARCH_TAG"
+  
+  #Prepare qemu
+  mkdir -p qemu
+  cd qemu
+  if [ ! -f qemu-"$TARGET_ARCH"-static ]; then
+    echo "Running in arch $(uname -m) and with TARGET_ARCH $TARGET_ARCH"
+    if [ "$TARGET_ARCH" = "amd64" -o "$TARGET_ARCH" = "$(uname -m)" ]; then
+      touch qemu-"$TARGET_ARCH"-static
+    else
+      # Prepare qemu
+      docker run --rm --privileged multiarch/qemu-user-static:register --reset
+      curl -L -o qemu-"$TARGET_ARCH"-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/"$QEMU_VERSION"/qemu-"$TARGET_ARCH"-static.tar.gz
+      tar xzf qemu-"$TARGET_ARCH"-static.tar.gz
+      rm qemu-"$TARGET_ARCH"-static.tar.gz
+    fi
+  fi
+  cd ..
+
+  #Build docker
   BASE=alpine
   if [ -n "$TARGET_IMG" ]; then
     BASE="$TARGET_IMG/$BASE"
@@ -58,6 +62,28 @@ if [ "$BUILD" = true ] ; then
   echo "Using base image: $BASE"
   docker build -t $REPO:$ARCH_TAG --build-arg BASE=$BASE --build-arg arch=$TARGET_ARCH .
 fi
+
+##############################
+
 if [ "$PUSH" = true ] ; then
+  echo "PUSHING TO DOCKER"
   docker push $REPO:$ARCH_TAG
+fi
+
+###############################
+
+if [ "$MANIFEST" = true ] ; then
+  echo "PUSHING MANIFEST for $ARCHS"
+  
+  for arch in $ARCHS; do
+    echo
+    echo "Add ${REPO}/${IMAGE}:${TAG}-${arch} to manifest ${REPO}/${IMAGE}:${TAG}"
+    echo
+    docker manifest create --amend ${REPO}/${IMAGE}:${TAG} ${REPO}/${IMAGE}:${TAG}-${arch}
+    docker manifest annotate       ${REPO}/${IMAGE}:${TAG} ${REPO}/${IMAGE}:${TAG}-${arch} --arch ${arch}
+  done
+
+  echo
+  echo "Push manifest angelnu/${IMAGE}:${TAG}"
+  docker manifest push ${REPO}/${IMAGE}:${TAG}
 fi
